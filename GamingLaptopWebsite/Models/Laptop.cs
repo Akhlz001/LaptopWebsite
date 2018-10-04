a6714ad03284b9ef5ad0ebdf21cd7fe99a853817
Zeeshan Akhlaq <21707955@bucks.ac.uk>
2018-10-04 13:07:23 +0100

    Modfied site master

---
+++ GamingLaptopWebsite/Models/Laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GamingLaptopWebsite.Models
+{
+    public class Laptop
+    {
+        public int LaptopID { get; set; }
+
+        public string Model { get; set; }
+    }
+}
